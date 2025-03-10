b26ad2a0f475ee1f80aa74977ff6744a0f6ea6a1
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-03-10 05:29:14 +0500

    Update delete.py

--- src/database/delete.py
+++ src/database/delete.py
@@ -1,9 +1,11 @@
 import sqlite3
 
-db = sqlite3.connect('database.sqlite3')
-sql = db.cursor()
-sql.execute("DELETE FROM users")  # Barcha ma'lumotlarni o‘chirish
-db.commit()
+conn = sqlite3.connect("database.sqlite3")  # SQLite bazasiga ulanish
+cursor = conn.cursor()
 
+cursor.execute("DELETE FROM users")  # Barcha ma'lumotlarni o‘chirish
+conn.commit()  # O‘zgarishlarni saqlash
 
 print("Barcha ma'lumotlar o‘chirildi!")
+
+conn.close()  # Ulanishni yopish
