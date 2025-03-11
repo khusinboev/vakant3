ee5ad69efdf73a0a2067942c4a7b5008a6d4c643
Windows 10 <adhambekhusinboyev@gmail.com>
2025-03-11 20:09:47 +0500

    Yanada yaxshilangandin xiyalima

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import sqlite3
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from pathlib import Path
+
+from src.middleware.middlewares import StatsMiddleware
+
 storage = MemoryStorage()
 
 adminStart = 1918760732
@@ -12,6 +15,8 @@ BASE_DIR = str(Path(__file__).resolve().parent)+"/src/database/database.sqlite3"
 TOKEN = "7855267108:AAFVmpmPaL58OF06mlnozB0YdHQDh-cs_r8"
 bot = Bot(token=TOKEN, parse_mode='html')
 dp = Dispatcher(bot=bot, storage=storage)
+dp.middleware.setup(StatsMiddleware())
+
 
 db = sqlite3.connect(BASE_DIR)
 sql = db.cursor()
