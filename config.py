d74c1cdcfb1aa480bf2702fc04cc9ca8f6ef8c3e
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-10-05 11:22:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ADMIN_IDS = [1918760732, 619839487, 5246872049]
 BASE_DIR = str(Path(__file__).resolve().parent) + "/src/database/database.sqlite3"
 
 # Token
-TOKEN = "5631190031:AAFwzY5z84yqZLoSk6nJ4oV_xYP8cm6yN2Q"
+TOKEN = "7855267108:AAG1DRIP6cpfe5VDVWd1b2HGlb6WWFaCxDo "
 
 # Bot va Dispatcher
 bot = Bot(
